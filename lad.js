3d50c880d90b892d9b66d22d51d6d0048a26eef5
thiagotaujiro <thiag_taujiro@hotmail.com>
2022-10-22 09:37:56 +1100

    news

--- lad.js
+++ lad.js
@@ -1,17 +1,9 @@
-var diceOne = document.getElementById("dice1");
-var comeOut = document.getElementById("roll");
-
-comeOut.onclick = function(){ rollDice();};
-
-function rollDice() {
-    var diceOne = Math.floor((Math.random() * 6) +1);
-
-    console.log(diceOne);
-
-    for(var i=1; i<= 6; i++) { diceOne.classList.remove("show-" +i);
-if (diceOne === i) { 
-    diceOne.classList.add("show-" +i);}
-}
-
-setTimeout(rollDice(), 1000);
+function rollDice () {
+    return Math.floor (Math.random () * 6);
+    for (let i = 0; i < 5; i++) console.log(rollDice () );
+    5
+    1
+    4
+    2
+    0
 }
